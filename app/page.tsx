f5ad86773d128000944750144bcad42d4b911e3a
Erick Tran <thelittlebotengineer@gmail.com>
2025-01-29 23:37:10 -0800

    feat(app): add sponsors section
    Added a sponsors section and styled it appropriately, and also added one image for the section

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,32 @@ import Title from "@/app/_components/title";
 import AboutBrief from "@/app/_components/aboutbrief";
 import MainHead from "@/app/_components/head";
 import Slogan from "@/app/_components/slogan";
+import Sponsors from "@/app/_components/sponsors";
 
 // Images
 import img from "@/public/images/FrontPageBanner.jpg";
 import img2 from "@/public/images/collage2.png";
 
+// Images
+import nasa from "@/public/logos/sponsors/NASA_logo.png";
+
+// Other imports
+import {StaticImageData} from "next/image";
+
+// Interfaces / Types
+type Sponsor = {
+    img: StaticImageData;
+    name: string;
+    link: string;
+}
+
 export default async function Home(): Promise<React.ReactElement> {
+    const sponsors: Sponsor[] = [{
+        img: nasa,
+        name: "NASA",
+        link: "https://www.nasa.gov/"
+    }]
+
     return (
         <div id={`${styles.homepage}`}>
             <MainHead headTitle={"Home"}/>
@@ -27,6 +47,7 @@ export default async function Home(): Promise<React.ReactElement> {
                 description={"We are Oxford Academy Robotics, a high school robotics team based in Cypress, California. We are dedicated to inspiring students to pursue careers in STEM fields through hands-on experience in robotics."}
                 title={"Our Goals"}
             />
+            <Sponsors sponsors={sponsors} title={"Our Sponsors"}/>
             <Footer/>
         </div>
     )
