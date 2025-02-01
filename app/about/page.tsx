26d69afa7af622c5941174092b2896505fbd3fe3
Erick Tran <thelittlebotengineer@gmail.com>
2025-02-01 12:35:30 -0800

    feat(project): minor style changes, add some stuff for about page
    Added some minor style changes, some images, and a section for the about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,6 +5,11 @@ import Navbar from "@/app/_components/navbar";
 import Footer from "@/app/_components/footer";
 import Title from "@/app/_components/title";
 import img1 from "@/public/images/AboutBanner.jpg";
+import VEXLogo from "@/public/logos/VEXLogo.jpg";
+import FTCLogo from "@/public/logos/FTCLogo.png";
+import FRCLogo from "@/public/logos/FRCLogo.png";
+
+import AboutRobotics from "@/app/about/_components/aboutRobotics";
 
 export default async function About(): Promise<React.ReactElement> {
     return (
@@ -16,6 +21,7 @@ export default async function About(): Promise<React.ReactElement> {
                 img1={img1}
                 bgMoveUp={50}
             />
+            <AboutRobotics img={[VEXLogo, FTCLogo, FRCLogo]} description={""}/>
             <Footer/>
         </div>
     )
