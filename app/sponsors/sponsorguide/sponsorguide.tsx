257a23101466d7512006b422cf3692dec52d27df
Erica Trans <thelittlebotengineer@gmail.com>
2025-02-28 19:30:06 -0800

    feat(project): add sponsorguide, modify footer and header
    Added sponsorguide (basically finished it), modified footer font and header font to be more
    consistent with the rest of the website

--- app/sponsors/sponsorguide/sponsorguide.tsx
+++ app/sponsors/sponsorguide/sponsorguide.tsx
@@ -6,11 +6,31 @@ type SponsorGuide = {
     children?: React.ReactNode;
 }
 
-export default async function SponsorGuide({ children }: SponsorGuide): Promise<React.ReactElement> {
+export default async function SponsorGuide({children}: SponsorGuide): Promise<React.ReactElement> {
     return (
         <div className={`${styles.sponsorGuide} ${TW_900}`}>
             <h1>How to Sponsor</h1>
-            { children }
+            <div className={styles.sponsorGuideContent}>
+                <div className={styles.sponsorGuideContentMain}>
+                    <div className={styles.sponsorGuideContentItem}>
+                        <h2>Step 1</h2>
+                        <p>Fill out the form below</p>
+                    </div>
+                </div>
+                <div className={styles.sponsorGuideContentMain}>
+                    <div className={styles.sponsorGuideContentItem}>
+                        <h2>Step 2</h2>
+                        <p>Wait for a response from our team</p>
+                    </div>
+                </div>
+                <div className={styles.sponsorGuideContentMain}>
+                    <div className={styles.sponsorGuideContentItem}>
+                        <h2>Step 3</h2>
+                        <p>Get started with your sponsorship!</p>
+                    </div>
+                </div>
+            </div>
+            {children}
         </div>
     )
 }
